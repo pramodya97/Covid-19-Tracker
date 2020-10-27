91e05384b14f60e3f4837d5ccd3a4efd00241026
pramodyahettiarachchi <pramodyahettiarachchi@gmail.com>
2020-10-28 00:25:52 +0530

    initialize project : covid-19 Tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className= "app__dropdown">
+        <Select
+         variant="outlined"
+         value="abc">
+           <MenuItem value="worldwide">Worldwide</MenuItem>
+           <MenuItem value="worldwide">Worldwide</MenuItem>
+           <MenuItem value="worldwide">Worldwide</MenuItem>
+           <MenuItem value="worldwide">Worldwide</MenuItem>
+           <MenuItem value="worldwide">Worldwide</MenuItem>
+        </Select>
+
+      </FormControl>
+
+     {/* Header */}
+     {/* Title + Dropdown */}
+
+     {/* Info box */}
+     {/* Info box */}
+     {/* Info box */}
+
+     {/* Table */}
+     {/* Graph */}
+
+     {/* Map */}
     </div>
   );
 }
